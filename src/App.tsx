90292cdbee0f427de5590b5ba670dc6e63c54a55
DanielyVasconcelos2024 <daniely.vasconcelos@academico.uncisal.edu.br>
2025-10-15 16:04:47 -0300

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/cuidar-de-quem-cuida">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
